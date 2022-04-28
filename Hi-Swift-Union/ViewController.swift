fdc821ace90854b6b0c1170d56cc3c3010f6e3eb
linlei <stoneuou@163.com>
2022-04-28 16:57:29 +0800

    Initial Commit

---
+++ Hi-Swift-Union/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Hi-Swift-Union
+//
+//  Created by stone on 2022/4/28.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
